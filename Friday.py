c60e0455b6e4843ec460a7956659bc9eb1f8a51e
Shaurya Anjney <shauryamanjneb@gmail.com>
2026-09-10 17:11:56 +0530

    Update Friday.py

--- Friday.py
+++ Friday.py
@@ -10,7 +10,7 @@ import time
 
 # ---------------- CONFIG ----------------
 GROQ_API_KEYS = st.secrets.get("GROQ_API_KEYS", [os.getenv("GROQ_API_KEY")])
-MODEL = "groq/compound"
+MODEL = "groq/compound-mini"
 
 # Stronger Silent API Rotation
 @st.cache_resource(show_spinner=False)
