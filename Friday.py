72f2a0a5603fcdb6c698a13b996397702cce9ea9
Shaurya Anjney <shauryamanjneb@gmail.com>
2026-09-10 17:44:03 +0530

    Update Friday.py

--- Friday.py
+++ Friday.py
@@ -10,7 +10,7 @@ import time
 
 # ---------------- CONFIG ----------------
 GROQ_API_KEYS = st.secrets.get("GROQ_API_KEYS", [os.getenv("GROQ_API_KEY")])
-MODEL = "qwen/qwen3-32"
+MODEL = "openai/gpt-oss-20b"
 
 # Stronger Silent API Rotation
 @st.cache_resource(show_spinner=False)
